892825ef34b585f8a22c75e4d0f4958e5b24b583
munal777 <munalpoudel3@gmail.com>
2025-06-07 21:01:09 +0545

    changes on dashboard view

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from item.models import Item
-from django.http import HttpResponse
+
 @login_required
 def dashboard(request):
     items = Item.objects.filter(created_by = request.user)
 
     return render(request, 'book/index.html', {
-        'items': items,
+        'items': items, 'username': request.user.username
     })
